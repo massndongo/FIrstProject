5d5ef6d5abec114f40dae86ec50111c3fbca6afc
massndongo86@gmail.com <massndongo86@gmail.com>
2023-10-30 17:07:49 +0000

    Exercice Etape 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Bonjour le monde !");
+        salutation();
+        calcSomme();
     }
+
+    public static void salutation(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Saisir votre nom: ");
+        String nom = sc.nextLine();
+        System.out.println("Bonjour, "+nom+" !");
+
+    }
+    public static void calcSomme(){
+        int somme = 0;
+        for (int i=1; i<=100; i++) {
+            somme += i;
+        }
+        System.out.println("La somme des nombres de 1 à 100 est égale à : "+somme);
+    }
+
+
 }
+
